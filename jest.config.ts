de770b240dbdee8837f4ceb9c7bc4412be9fdfd8
Ayushi Sharma <ayu704sharma32@gmail.com>
2026-05-21 19:50:26 +0530

    fix: add jest globals import to fix type errors

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from 'jest'
 
 const config: Config = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  transform: {},
 }
 
 export default config
